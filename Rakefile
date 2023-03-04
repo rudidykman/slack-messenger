ea759396d37c14dc2f7d0ec184330eed124168cb
Rudi Dykman <rudi@root.co.za>
2023-03-04 07:53:29 +0200

    Add local dev tools

--- Rakefile
+++ Rakefile
@@ -1,6 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'lib/handler'
+require_relative 'lib/helpers'
+
 task :test do
   ENV['RUBY_ENV'] = 'test'
   ruby 'test/handler_test.rb'
+end
+
+task :invoke do
+  payload = File.open('payload.json').read
+  alert(event: { 'body' => payload }, context: {})
+end
+
+task :get_channel_id, [:channel_name] do |task, args|
+  get_channel_id(args[:channel_name])
 end
--- lib/slack_api.rb
+++ lib/slack_api.rb
@@ -8,17 +8,16 @@ class SlackApi
   def initialize
     @base_uri = 'https://slack.com/api'
     @token = ENV['SLACK_TOKEN']
-    @channel_id = ENV['SLACK_CHANNEL_ID']
   end
 
-  def send_message(message)
+  def send_message(message, channel_id = ENV['SLACK_CHANNEL_ID'])
     return puts 'Sending simulated Slack message...' if ENV['RUBY_ENV'] == 'test'
 
-    response = post('chat.postMessage', { channel: @channel_id, text: message })
-    response_body = JSON.parse(response.read_body)
-    return if response.code == '200' && response_body['ok']
+    post('chat.postMessage', { channel: channel_id, text: message })
+  end
 
-    raise "Could not send Slack message. Response from Slack: #{response_body}"
+  def get_channels
+    get('conversations.list')['channels']
   end
 
   private
@@ -29,6 +28,24 @@ class SlackApi
       'Authorization': "Bearer #{@token}",
       'Content-Type': 'application/json'
     }
-    Net::HTTP.post(uri, body.to_json, headers)
+    response = Net::HTTP.post(uri, body.to_json, headers)
+    parse_response(response)
+  end
+
+  def get(endpoint)
+    uri = URI.parse("#{@base_uri}/#{endpoint}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    request = Net::HTTP::Get.new(uri)
+    request['Authorization'] = "Bearer #{@token}"
+    response = http.request(request)
+    parse_response(response)
+  end
+
+  def parse_response(response)
+    response_body = JSON.parse(response.read_body)
+    return response_body if response.code == '200' && response_body['ok']
+
+    raise "Slack API request failed. Response from Slack: #{response_body}"
   end
 end
